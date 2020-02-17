67e117b81a4ff2596da54826b1d766f3a3777c9e
Michael Ochnicki <ochnicki.michael@gmail.com>
2020-02-17 20:06:53 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -41,5 +41,6 @@ def naming_system
   #  Species
   # So, if we have the "Kingdom" node we should be able to "tunnel" into the
   # HoH all the way to Species!
-  bigHash = PORTION_1[:sub_category][:sub_category][:sub_category] = PORTION_2
+  bigHash = PORTION_1
+  bigHash[:sub_category][:sub_category][:sub_category] = PORTION_2
 end
