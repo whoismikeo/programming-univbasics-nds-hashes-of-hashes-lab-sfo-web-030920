971a9cfb60ef4ed320810785efb83605b480f441
Michael Ochnicki <ochnicki.michael@gmail.com>
2020-02-17 20:06:32 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -14,9 +14,9 @@ PORTION_1 = {
   }
 }
 # 
-# PORTION_2 = {
-#   label: "Order"
-# }
+PORTION_2 = {
+  label: "Order"
+}
 # 
 # PORTION_3 = {
 #   label: "Family",
@@ -41,5 +41,5 @@ def naming_system
   #  Species
   # So, if we have the "Kingdom" node we should be able to "tunnel" into the
   # HoH all the way to Species!
-  PORTION_1
+  bigHash = PORTION_1[:sub_category][:sub_category][:sub_category] = PORTION_2
 end
